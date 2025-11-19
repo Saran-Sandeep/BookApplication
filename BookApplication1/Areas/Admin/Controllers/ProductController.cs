01db6a42be00165e497f2419b60483a0a6471c07
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-19 14:51:56 +0530

    Combined Create, Update into Upsert

--- BookApplication1/Areas/Admin/Controllers/ProductController.cs
+++ BookApplication1/Areas/Admin/Controllers/ProductController.cs
@@ -11,9 +11,11 @@ namespace BookApplication1.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -36,24 +38,89 @@ namespace BookApplication1.Areas.Admin.Controllers
             return View(productDetails);
         }
 
-        public IActionResult Create()
+        [HttpGet("Admin/Product/Upsert")]
+        [HttpGet("Admin/Product/Upsert/{productId}")]
+        public IActionResult Upsert(int? productId)
         {
-            return View();
+            ProductVM productVM = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(u =>
+             new SelectListItem
+             {
+                 Text = u.Name,
+                 Value = u.Id.ToString()
+             });
+            productVM.CategoryList = CategoryList;
+            if (productId == null || productId == 0) {
+                // Create /Insert
+                productVM.Product = new();
+            }
+            else
+            {
+                // Edit /Update
+                Product productDetails = _unitOfWork.ProductRepository.Get(p => p.Id == productId);
+                if (productDetails == null) { return NotFound(); }
+                productVM.Product = productDetails;
+            }
+
+             return View(productVM);
+
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Upsert(ProductVM productVM, IFormFile formFile)
         {
             if (ModelState.IsValid)
             {
-                product.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
-                product.UpdatedAt = DateOnly.FromDateTime(DateTime.Now);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (formFile != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
 
-                _unitOfWork.ProductRepository.Add(product);
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageURL))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageURL);
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        formFile.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageURL = @"images\product\" + fileName;
+                }
+
+                if(productVM.Product.Id == 0)
+                {
+                    productVM.Product.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+                    productVM.Product.UpdatedAt = DateOnly.FromDateTime(DateTime.Now);
+
+                    _unitOfWork.ProductRepository.Add(productVM.Product);
+                }
+                else
+                {
+                    productVM.Product.UpdatedAt = DateOnly.FromDateTime(DateTime.Now);
+                    _unitOfWork.ProductRepository.Update(productVM.Product);
+                }
+
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(u =>
+                new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+
+                return View(productVM);
+            }
         }
 
         [HttpPost("Admin/Product/Delete/{productId}")]
@@ -71,27 +138,6 @@ namespace BookApplication1.Areas.Admin.Controllers
             _unitOfWork.ProductRepository.Remove(productDetails);
             _unitOfWork.Save();
             return RedirectToAction("Index", "Product");
-        }
-
-        [HttpGet("Admin/Product/Edit/{productId}")]
-        public IActionResult Edit(int? productId)
-        {
-            if(productId == null || productId == 0) { return NotFound(); }
-            Product productDetails = _unitOfWork.ProductRepository.Get(p => p.Id==productId);
-            if (productDetails == null) { return NotFound(); }
-            return View(productDetails);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Update(product);
-                _unitOfWork.Save();
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
         }
     }
 }
