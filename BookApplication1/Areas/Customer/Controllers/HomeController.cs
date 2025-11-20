226cb3c19fde1cd99cfb3f9781a129a0af40fda5
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-20 10:42:18 +0530

    Add/Index and Details Views in Home

--- BookApplication1/Areas/Customer/Controllers/HomeController.cs
+++ BookApplication1/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using BookApplication1.DataAccess.Repository.IRepository;
 using BookApplication1.Models;
 using BookApplication1.Models.Models;
+using BookApplication1.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookApplication1.Areas.Customer.Controllers
@@ -9,15 +11,30 @@ namespace BookApplication1.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> productList = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category").ToList();
+            return View(productList);
+        }
+
+        [HttpGet("Customer/Home/Details/{productId}")]
+        public IActionResult Details(int? productId)
+        {
+            if (productId == null || productId == 0)
+            {
+                return NotFound();
+            }
+            Product productDetails = _unitOfWork.ProductRepository.Get(p => p.Id == productId, includeProperties: "Category");
+
+            return productDetails == null ? NotFound() : View(productDetails);
         }
 
         public IActionResult Privacy()
