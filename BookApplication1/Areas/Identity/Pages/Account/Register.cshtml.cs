5e25a105c34246ec9b128f9eb06bc7bd8eacf26c
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-21 12:02:08 +0530

    Add/Register new users as application user instead as identity user.

--- BookApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using BookApplication1.Models.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -154,11 +155,11 @@ namespace BookApplication1.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
