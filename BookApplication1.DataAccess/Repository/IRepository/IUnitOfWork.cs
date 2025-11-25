c581c5db1447ddc1f971e18bac54126be2a16052
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-25 14:48:20 +0530

    Add/Company Table Repository and Unit of Work

--- BookApplication1.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BookApplication1.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace BookApplication1.DataAccess.Repository.IRepository
     {
         ICategoryRepository CategoryRepository { get; }
         IProductRepository ProductRepository { get; }
+        ICompanyRepository CompanyRepository { get; }
         void Save();
     }
 }
--- BookApplication1.DataAccess/Repository/UnitOfWork.cs
+++ BookApplication1.DataAccess/Repository/UnitOfWork.cs
@@ -13,13 +13,14 @@ namespace BookApplication1.DataAccess.Repository
     {
         private readonly ApplicationDbContext _db;
         public ICategoryRepository CategoryRepository { get; set; }
-
         public IProductRepository ProductRepository { get; set; }
+        public ICompanyRepository CompanyRepository { get; set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             CategoryRepository = new CategoryRepository(_db);
             ProductRepository = new ProductRepository(_db);
+            CompanyRepository = new CompanyRepository(_db);
         }
         
         
