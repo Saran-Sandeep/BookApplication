b81aa45cf00aeb44515f820bca56c7c15b95a07b
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-25 15:37:05 +0530

    Add/Compant FK For Applicaiton user and migrations for it

--- BookApplication1.Models/Models/ApplicationUser.cs
+++ BookApplication1.Models/Models/ApplicationUser.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace BookApplication1.Models.Models
 {
@@ -16,7 +18,10 @@ namespace BookApplication1.Models.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
-
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
 
     }
 }
