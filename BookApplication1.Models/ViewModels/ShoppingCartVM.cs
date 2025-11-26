3621bedcb2f12b57cf9e727b899f1f37d011659f
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-26 11:20:50 +0530

    Add/Shopping Cart VM

---
+++ BookApplication1.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookApplication1.Models.Models;
+
+namespace BookApplication1.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList  { get; set; }
+        public double OrderTotal { get; set; }
+
+    }
+}
