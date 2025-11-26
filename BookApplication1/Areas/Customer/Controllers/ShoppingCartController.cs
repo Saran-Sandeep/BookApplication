d0a3d2a132dd4d38b02420d970d044f66f04c45c
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-26 11:22:05 +0530

    Add/Shopping Cart Controller and Get Shopping Cart VM From DB using unit of Work

---
+++ BookApplication1/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using BookApplication1.DataAccess.Repository.IRepository;
+using BookApplication1.Models.Models;
+using BookApplication1.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApplication1.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            IEnumerable<ShoppingCart> shoppingCartList = _unitOfWork.ShoppingCartRepository
+                .GetAll(i => i.ApplicationUserId == userId, includeProperties: "Product")
+                .ToList();
+            ShoppingCartVM shoppingCartVM = new()
+            {
+                ShoppingCartList = shoppingCartList,
+                OrderTotal = shoppingCartList.Sum(i => i.Product.Price * i.count)
+            };
+            return View(shoppingCartVM);
+        }
+    }
+}
