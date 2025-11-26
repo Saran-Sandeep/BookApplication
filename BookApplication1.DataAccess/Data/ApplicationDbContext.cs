8b4282ccc9b2ef8eb0878233518e11ae539acd38
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-26 14:59:32 +0530

    Add/OrderHeader and OrderDetails Tables and Updated Migrations

--- BookApplication1.DataAccess/Data/ApplicationDbContext.cs
+++ BookApplication1.DataAccess/Data/ApplicationDbContext.cs
@@ -14,8 +14,8 @@ namespace BookApplication1.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-        //public DbSet<OrderHeader> OrderHeaders { get; set; }
-        //public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
