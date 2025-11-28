668817048a37cb9c53c72bcea117773a1a384732
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-28 09:57:59 +0530

    Add/RazorPay and its Api keys configuration

---
+++ BookApplication1.Utility/RazorPaySettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApplication1.Utility
+{
+    public class RazorPaySettings
+    {
+        public string SecretKey { get; set; } = null!;
+        public string PublishableKey { get; set; } = null!;
+    }
+}
--- BookApplication1/Program.cs
+++ BookApplication1/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.Configure<RazorPaySettings>(builder.Configuration.GetSection("RazorPay"));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
