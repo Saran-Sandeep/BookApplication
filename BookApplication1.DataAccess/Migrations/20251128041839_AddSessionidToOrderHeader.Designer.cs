06e189671f47ca9d666acf949fee9ab657075d98
Saran Sandeep Yedla <saransandeepyedla03@gmail.com>
2025-11-28 09:58:28 +0530

    Add/SessionId to Order Header Table and its migrations

--- BookApplication1.DataAccess/Migrations/20251128041839_AddSessionidToOrderHeader.Designer.cs
+++ BookApplication1.DataAccess/Migrations/20251128041839_AddSessionidToOrderHeader.Designer.cs
@@ -4,16 +4,19 @@ using BookApplication1.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BookApplication1.Migrations
+namespace BookApplication1.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251128041839_AddSessionidToOrderHeader")]
+    partial class AddSessionidToOrderHeader
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
